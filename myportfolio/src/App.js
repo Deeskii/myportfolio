e139ff6e1c0f8b5caa354ebef3594665a1d3ed3a
Dee Ragoo <deeskii32443@gmail.com>
2020-06-11 18:01:28 -0400

    added bootstrap

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import {Navbar} from './mycomponents/navbar';
 import './App.css';
 
 function App() {
   return (
+
     <div style={{textAlign: 'center'}}>
+      <Navbar />
    <h1>Damarius Ragoo </h1>
    </div>
   );
---
+++ myportfolio/src/mycomponents/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Navbar extends React.Component {
+render () {
+    return (
+<div>
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">Disabled</a>
+  </li>
+</ul>
+</div>
+    );
+}
+}
