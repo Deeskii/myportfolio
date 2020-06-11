b9d54d688509a5f6c94cff7f2bf5d54fa7c63779
Dee Ragoo <deeskii32443@gmail.com>
2020-06-11 06:40:23 -0400

    React init setup

---
+++ myportfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div style={{textAlign: 'center'}}>
+   <h1>Damarius Ragoo </h1>
+   </div>
+  );
+}
+
+export default App;
